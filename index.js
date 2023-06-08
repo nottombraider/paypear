04f2d15a335818fcb848a4bcc61fd7b23bf3bc37
nottombraider <larysa.yavorska@outlook.com>
2023-06-08 10:31:24 +0300

    [feat] - add media for home page

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
     emailjs.init('s4A9YA0fh6oUVAPy6');
 })();
 
-new Swiper(".swiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
+new Swiper("#integration-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 24,
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -14,6 +15,34 @@ new Swiper(".swiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+});
+
+new Swiper("#industries-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 24,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        650: {
+            slidesPerView: 2,
+            spaceBetween: 16
+        },
+        850: {
+            slidesPerView: 3,
+            spaceBetween:24
+        }
+    }
 });
 
 document.getElementById('contact-form').addEventListener('submit', (event) => {
